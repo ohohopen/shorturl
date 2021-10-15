96f5653f069463008f123029a555bdae8f08760c
ohohopen <ohohopen@gmail.com>
2021-10-15 08:44:13 +0800

    測試能否正常匯入所有資料

--- app.js
+++ app.js
@@ -4,20 +4,21 @@ const exphbs = require("express-handlebars");
 const app = express();
 require("./config/mongoose");
 const bodryParser = require("body-parser");
-const { urlencoded } = require("express");
 const ShortUrl = require("./models/shorturl");
+const randomStr = require("./randomStr");
 app.use(bodryParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }));
 app.set("view engine", "hbs");
 app.get("/", (req, res) => {
-	res.render("index");
+	ShortUrl.find()
+		.lean()
+		.then((shorturls) => {
+			res.render("index", { shorturls });
+		})
+		.catch((error) => console.log(error));
 });
-app.post("/shorturl", (req, res) => {
-	console.log(req.body.webUrl);
-	const adderss = req.body.webUrl;
-	res.send("post成功");
-});
+
 app.listen(port, () => {
 	console.log("app is connecting.");
 });
